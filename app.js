114bb9ed09e5478e098f5fb023a2cd36f3755b6d
Michael Murry <michaelmurry49@gmail.com>
2020-05-10 23:17:50 -0700

    heroku please

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const passport = require('passport')
 
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
+
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to mongoDB"))
@@ -16,6 +24,8 @@ require("./config/passport")(passport)
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+
+
 
 app.use(bodyParser.json());
 
